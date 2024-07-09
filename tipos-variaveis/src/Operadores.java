1ea8e9317ac16926b57c50011c7fd992f83a6b40
LucasGeneroso15 <lucas.siilva12@live.com>
2024-07-09 12:31:56 -0300

    Aula Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,51 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+       /* PARTE  1
+            String nomeCompleto = "Linguagem " + "JAVA";
+            System.out.println(nomeCompleto);
+       */ 
+
+        /* PARTE 3        
+            int numero = 5;
+            numero = - numero;
+
+            System.out.println(numero);
+
+            // numero = - numero;
+            numero = numero * -1;
+            System.out.println(numero);
+        */
+        
+        /* PARTE 4        
+            int numero = 5;
+            numero = numero + 2;
+            System.out.println(numero);
+            numero ++;
+            System.out.println(numero);
+            System.out.println(++ numero);
+            System.out.println (-- numero);
+
+            boolean variavel = true;
+            System.out.println(variavel);
+            variavel = !variavel;
+            System.out.println(variavel);
+        */
+        /* Parte 5 
+        
+                int a = 5;
+                int b =6;
+
+                //if (a==b)
+                    resultado = "verdadeiro";
+                else
+                    resultado = "falso";
+
+                System.out.println(resultado);    //
+                
+                String resultado = a==b ? "verdadeiro" : "false";
+
+                System.out.println(resultado);       
+        */
+
+    }
+}
