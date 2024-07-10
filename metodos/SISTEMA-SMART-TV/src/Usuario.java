d84c0250c00c408dcc5018ab6d8ace08681c522d
LucasGeneroso15 <lucas.siilva12@live.com>
2024-07-10 18:21:27 -0300

    Modulo Métodos (proj. SmartTv)

---
+++ metodos/SISTEMA-SMART-TV/src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Tv ligada? "+ smartTv.ligada);
+        System.out.println("Canal atual: "+ smartTv.canal);
+        System.out.println("Volume atual: "+ smartTv.volume);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual: "+ smartTv.canal);
+
+        System.out.println("Novo Status - Tv ligada? "+ smartTv.ligada);
+
+    }
+}
