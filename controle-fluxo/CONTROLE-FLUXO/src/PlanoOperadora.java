f0e0b8e1308265803e92f57fe8708cf835d499b3
LucasGeneroso15 <lucas.siilva12@live.com>
2024-07-12 17:48:20 -0300

    Controle de Fluxo

---
+++ controle-fluxo/CONTROLE-FLUXO/src/PlanoOperadora.java
@@ -0,0 +1,15 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T"; // T - Turbo; M - Midea; B - Basic;
+
+        switch (plano) {
+            case "T":
+                System.out.println("5Gb Youtube");
+            case "M":
+                System.out.println("Whats e Instagram Grátis");
+            case "B":
+                System.out.println("100 Minutos de Ligação");
+        }
+
+    }
+}
